78c7277dd5e6ed8ed401bc498202429ffe0f413e
Miguel <rgomiguel17@gmail.com>
2025-02-17 10:40:53 -0300

    added spring web

--- demo/src/main/java/com/fornempr/demo/DemoApplication.java
+++ demo/src/main/java/com/fornempr/demo/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.fornempre.demo;
+package com.fornempr.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- demo/src/test/java/com/fornempr/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/fornempr/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fornempre.demo;
+package com.fornempr.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
