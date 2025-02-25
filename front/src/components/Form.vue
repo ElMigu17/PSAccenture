6b5786d9dcd3b5f607a39cb730d71dd001eac278
Miguel <rgomiguel17@gmail.com>
2025-02-24 21:10:51 -0300

    formatação + filtragem de fornecedor

--- front/src/components/Form.vue
+++ front/src/components/Form.vue
@@ -1,10 +1,10 @@
   <script setup>
-import { defineProps } from 'vue';
+  import { defineProps } from 'vue';
 
-const cnpj = defineModel('cnpj');
-const nomeFantasia = defineModel('nomeFantasia');
-const cep = defineModel('cep');
-const id = defineModel('id');
+  const cnpj = defineModel('cnpj');
+  const nomeFantasia = defineModel('nomeFantasia');
+  const cep = defineModel('cep');
+  const id = defineModel('id');
 
   const props = defineProps({
     dataFornecedoresMarcados: {
@@ -26,70 +26,74 @@ const id = defineModel('id');
         <input placeholder="00000-000" maxlength="9" v-mask="'#####-###'" type="text" v-model="cep" />
         <p>Fornecedor:</p>
         <div v-if="props.dataFornecedoresMarcados && props.dataFornecedoresMarcados.length > 0">
-          <label v-for="forn in props.dataFornecedoresMarcados" :key="forn.id" :for="forn.id">
-            <input 
-              type="checkbox"
-              :value="forn.id"
-              :id="forn.id" 
-              :name="forn.id"
-              :checked="forn.check"
-              @click="forn.check = !forn.check"
-            />
-            {{ forn.nome }} - {{ forn.is_pessoa_fisica ? forn.cpf : forn.cnpj }}
-            <br/>
-          </label>
+          <input type="text" v-model="filtro" />
+          <div v-for="forn in props.dataFornecedoresMarcados">
+            <label
+              v-if="filtro === '' || filtro === null || filtro !==null && (forn.is_pessoa_fisica ? forn.cpf.toString().includes(filtro) : forn.cnpj.includes(filtro) || forn.nome.includes(filtro))"
+              :key="forn.id" :for="forn.id">
+              <input type="checkbox" :value="forn.id" :id="forn.id" :name="forn.id" :checked="forn.check"
+                @click="forn.check = !forn.check" />
+              {{ forn.nome }} - {{ forn.is_pessoa_fisica ? forn.cpf : forn.cnpj }}
+              <br />
+            </label>
+          </div>
         </div>
         <br />
         <div class="buttons">
-        <input class="button" type="submit" @click="$emit('edit-todo', event)" />
-        <input class="close-button button" type="button" value="Close" @click="$emit('close', event)" />
-      </div>
+          <input class="button" type="submit" @click="$emit('edit-todo', event)" />
+          <input class="close-button button" type="button" value="Close" @click="$emit('close', event)" />
+        </div>
       </form>
     </div>
   </template>
 
 
-  <script>
+<script>
 
 
-  export default {
-  };
-  </script>
-
-  <style scoped>
-  .modal {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    background-color: rgba(0, 0, 0, 0.3);
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export default {
+  data() {
+    return {
+      filtro: null
+    }
   }
+};
+</script>
 
-  .form {
-    background-color: #FDFDFD;
-    padding: 20px 30px;
-    border-radius: 10px;
-    border: 3px solid black;
-  }
+<style scoped>
+.modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
-  .button {
-    border-radius: 30px;
-    margin-top: 15px;
-    background-color: black;
-    color: white;
-    border: 2px solid black;
-    padding: 3px 8px;
-    cursor: pointer;
-  }
+.form {
+  background-color: #FDFDFD;
+  padding: 20px 30px;
+  border-radius: 10px;
+  border: 3px solid black;
+}
 
-  .buttons{
-    display: flex;
-    justify-content: space-around;
-    width: 100%;
+.button {
+  border-radius: 30px;
+  margin-top: 15px;
+  background-color: black;
+  color: white;
+  border: 2px solid black;
+  padding: 3px 8px;
+  cursor: pointer;
+}
 
-  }
-  </style>
+.buttons {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+
+}
+</style>
